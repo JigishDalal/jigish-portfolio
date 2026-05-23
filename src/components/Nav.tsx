9585547ec0e130425f5d9064929b7aec3f400986
jigishdalal <jigishdalal@gmail.com>
2026-05-23 19:58:28 +0530

    comment admin

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -72,7 +72,7 @@ export default function Nav() {
               {link.label}
             </Link>
           ))}
-          {<Link
+          {/* {<Link
             to="/admin"
             style={{
               marginLeft: '8px',
@@ -88,7 +88,7 @@ export default function Nav() {
               letterSpacing: '0.02em',
             }}
           >
-          </Link>}
+          </Link>} */}
         </div>
 
         {/* Mobile burger */}
